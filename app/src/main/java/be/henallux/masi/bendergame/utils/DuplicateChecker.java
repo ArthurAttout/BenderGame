d8eee415ce126305a35a585d596de9ef6e4117b2
Unknown <blast.off2010@hotmail.com>
2018-10-31 10:37:03 +0100

    Continued branch

--- app/src/main/java/be/henallux/masi/bendergame/utils/DuplicateChecker.java
+++ app/src/main/java/be/henallux/masi/bendergame/utils/DuplicateChecker.java
@@ -1,11 +1,5 @@
 package be.henallux.masi.bendergame.utils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 
 /**
